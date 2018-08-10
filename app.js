6285e219aacba00fdbdb93e265a058e641180f5b
Patty <patty.fresonke@forumcomm.com>
2018-08-10 14:07:33 -0500

    trying to add click count

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
   let catList = document.getElementById("catList");
   let catArea = document.getElementById("catArea");
   let catNames = ["Sherbert", "Melon", "Bombpop", "Donut", "Wonton"];
+  let catClicked = 0;
 
   let catNameHeader = document.createElement('h2');
-    catNameHeader.setAttribute("id", "catHeader");
+      catNameHeader.setAttribute("id", "catHeader");
+
+  let catCounterContainer = document.createElement('p');
+      catCounterContainer.setAttribute("id", "catCounter");
 
   catArea.appendChild(catNameHeader);
+  catArea.appendChild(catCounterContainer);
 
   for (let name = 0; name < catNames.length; name++) {
     let catName = catNames[name];
@@ -21,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function() {
         catButton.innerHTML = catName;
         catButton.className = "catNameListItem";
         catButton.setAttribute("id", "cat" + catName);
-        catButton.setAttribute("onclick", "showCat('"+catName+"');");
+        catButton.setAttribute("onclick", "showCat('" + catName + "', '" + catClicked++ + "');");
 
     catList.appendChild(catButton);
   }
@@ -30,17 +35,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-function showCat(nameofcat){
-  document.getElementById('catHeader').innerHTML = nameofcat;
-
+function showCat(nameofcat, count){
   document.getElementById('catPhotoArea').src='cat' + nameofcat + '.jpg';
   document.getElementById('catPhotoArea').className ='cat' + nameofcat;
+
+  document.getElementById('catHeader').innerHTML = nameofcat;
+
+  document.getElementById('catCounter').innerHTML = "Number of clicks: " + count;
 }
 
 
 
 
-
+/*
 var count0 = 0;
 allCats0.addEventListener('click', function() {
   count0++
@@ -54,4 +61,4 @@ allCats1.addEventListener('click', function() {
   var clickCount = document.getElementById('clickCounter' + 1);
   clickCount.innerHTML = "Number of clicks: " + count1;
 }, false);
-
+*/
